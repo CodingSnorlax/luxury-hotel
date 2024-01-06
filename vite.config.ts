eb0c7df80685fa5e373e9d0d3228036cb77db8e2
Karen Huang <thinknfix2021@gmail.com>
2024-01-07 00:01:06 +0800

    add: 加入custom_variables.scss 檔案

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // 開發中 or 產品
+  base: process.env.NODE_ENV === 'production' ? '/luxury-hotel/' : '/',
+  plugins: [
+    react()
+  ],
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, "node_modules/bootstrap")
+    }
+  },
 })
