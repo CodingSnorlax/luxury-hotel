1099f4ec6bc18cb61dc1b24a386fb98a706a728a
Karen Huang <thinknfix2021@gmail.com>
2024-01-07 23:26:15 +0800

    fix: 改為 hashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@
 import { Routes, Route } from 'react-router-dom'
 import { HomePage } from './views/HomePage/HomePage'
 import { LoginPage } from './views/LoginPage/LoginPage'
+import { NotFoundPage } from './views/NotFoundPage/NotFoundPage'
 
 export const App: React.FC = () => {
   return (
     <>
       <div className='App'>
           <h1 className='bg-primary'>這裡放 HeaderComponent</h1>
-          {/* 路由表 */}
+          {/* 註冊路由表 */}
           <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='login' element={<LoginPage />} />
+            <Route path='*' element={<NotFoundPage />} />
           </Routes>
           <h1 className='bg-dark text-light'>這裡放 FooterComponent</h1>
       </div>
---
+++ src/views/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const NotFoundPage: React.FC = () => {
+    return(
+        <>
+            <h1>找不到此頁面！</h1>
+        </>
+    )
+}
