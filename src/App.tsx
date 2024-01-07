9f9c4037c525e0608dea7358e6f3c12359aef366
Karen Huang <thinknfix2021@gmail.com>
2024-01-07 22:33:01 +0800

    feat: 加入react-router 及部分專案結構

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import { useState } from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
+import { HomePage } from './views/HomePage/HomePage'
+import { LoginPage } from './views/LoginPage/LoginPage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Vite + React cccc</h1>
-      <div className="card">
-        <button className='btn btn-primary mb-24' onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className='App'>
+          <h1 className='bg-primary'>這裡放 HeaderComponent</h1>
+          {/* 路由表 */}
+          <Routes>
+            <Route path='/home' element={<HomePage />}/>
+            <Route path='/login' element={<LoginPage />}/>
+          </Routes>
+          <h1 className='bg-dark text-light'>這裡放 FooterComponent</h1>
       </div>
-      
     </>
   )
 }
