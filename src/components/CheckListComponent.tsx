dd99abd33941be57bf98726d8976f3901c81524d
MSI\user <s97011501@gmail.com>
2024-01-24 03:27:42 +0800

    fix: 調整 CheckListComponent 排版

--- src/components/CheckListComponent.tsx
+++ src/components/CheckListComponent.tsx
@@ -13,7 +13,7 @@ export const CheckListComponent: React.FC<CheckListComponentProps> = ({
     <ul className="list-unstyled bg-light rounded p-6 pb-4 row g-0">
       {checkListArr.map((item, index) => (
         <li
-          className="col-6 col-md-4 col-lg-3 col-xl-2 mb-2 me-md-4 me-lg-6 me-xl-10"
+          className="col-6 col-md-4 col-lg-3 col-xl-2 mb-2 me-md-4 me-lg-6"
           key={index}
         >
           <div className="d-flex align-items-center">
