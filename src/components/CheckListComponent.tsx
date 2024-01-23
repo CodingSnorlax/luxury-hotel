d372ca6477caae1fbc55bdac9cfbe5ea0df1988b
MSI\user <s97011501@gmail.com>
2024-01-24 03:18:16 +0800

    fix: 修正 CheckListComponent 排版

--- src/components/CheckListComponent.tsx
+++ src/components/CheckListComponent.tsx
@@ -1,23 +1,27 @@
 import React from "react";
-import checkIcon from "../assets/icon/check.svg";
+import Icon from "@mdi/react";
+import { mdiCheck } from "@mdi/js";
 
 interface CheckListComponentProps {
   checkListArr: string[];
 }
 
-export const CheckListComponent: React.FC<CheckListComponentProps> = ({ checkListArr }) => {
-    return (
-      <ul className="list-unstyled row bg-light rounded p-6">
-        {checkListArr.map((item, index) => (
-          // 每5項換一個新的li元素
-        <React.Fragment key={index}>
-        {index > 0 && index % 5 === 0 && <div className="w-100 mb-4" key={`divider-${index}`} />}
-        <li className="col-4 col-md-2 me-md-4">
-          <img src={checkIcon} className="me-2" alt={item} />
-          <span>{item}</span>
+export const CheckListComponent: React.FC<CheckListComponentProps> = ({
+  checkListArr,
+}) => {
+  return (
+    <ul className="list-unstyled bg-light rounded p-6 pb-4 row g-0">
+      {checkListArr.map((item, index) => (
+        <li
+          className="col-6 col-md-4 col-lg-3 col-xl-2 mb-2 me-md-4 me-lg-6 me-xl-10"
+          key={index}
+        >
+          <div className="d-flex align-items-center">
+            <Icon path={mdiCheck} size={1} className="text-primary me-2" />
+            <span>{item}</span>
+          </div>
         </li>
-      </React.Fragment>
-        ))}
-      </ul>
-    );
-  };
+      ))}
+    </ul>
+  );
+};
