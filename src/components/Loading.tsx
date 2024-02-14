98e512932b0d15325cfe507af39e0b2f81c53601
Vic <syuan0212@gmail.com>
2024-02-14 22:55:33 +0800

    feat: 會員頁增加通知與讀取效果

---
+++ src/components/Loading.tsx
@@ -0,0 +1,9 @@
+function Loading() {
+  return (
+    <div className="spinner-border text-light spinner-border-sm" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </div>
+  );
+}
+
+export default Loading;
